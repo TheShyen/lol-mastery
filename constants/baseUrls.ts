b64424d40221b11122a9439bca25fbdc1b6ec2e4
TheShyen <ruslankaslesh@mail.ru>
2024-01-26 13:32:30 +0500

    fix for deploy

--- constants/baseUrls.ts
+++ constants/baseUrls.ts
@@ -1,5 +1,5 @@
 import {version} from "~/constants/version";
 import {UrlConstructor} from "#imports";
-const BASE_URL = `https://ddragon.leagueoflegends.com/cdn/${version}/`
+const BASE_URL = `https://ddragon.leagueoflegends.com/cdn/${version()}/`
 export const BASE_URL_DATA = UrlConstructor(BASE_URL, 'data/')
 export const BASE_URL_IMAGE = UrlConstructor(BASE_URL, 'img/')
